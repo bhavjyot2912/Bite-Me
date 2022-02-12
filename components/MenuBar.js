d78f8dd593cc64abdee0ac8eea20d77b64c5dcad
bhavjyot2912 <Bhavjyot.Singh@iiitb.org>
2022-02-12 12:42:48 +0530

    menu added

--- components/MenuBar.js
+++ components/MenuBar.js
@@ -1,10 +1,56 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, ScrollView } from "react-native";
+import React from "react";
+
+const items = [
+  {
+    image: require("/home/bhavjyot/Desktop/BiteMe/front/images/images/shopping-bag.png"),
+    text: "Takeaway",
+  },
+  {
+    image: require("/home/bhavjyot/Desktop/BiteMe/front/images/images/soft-drink.png"),
+    text: "Soft Drinks",
+  },
+  {
+    image: require("/home/bhavjyot/Desktop/BiteMe/front/images/images/bread.png"),
+    text: "Bakery Items",
+  },
+  {
+    image: require("/home/bhavjyot/Desktop/BiteMe/front/images/images/fast-food.png"),
+    text: "Fast Foods",
+  },
+  {
+    image: require("/home/bhavjyot/Desktop/BiteMe/front/images/images/coffee.png"),
+    text: "Coffee & Tea",
+  },
+  {
+    image: require("/home/bhavjyot/Desktop/BiteMe/front/images/images/desserts.png"),
+    text: "Desserts",
+  },
+];
 
 export default function MenuBar() {
   return (
-    <View>
-      <Text>Explore Menu</Text>
+    <View style={{marginTop: 7, backgroundColor: "#ADD8E6"}}>
+      <ScrollView horizontal>
+        {items.map((item, index) => (
+          <View
+            key={index}
+            style={{ marginLeft: 10, marginTop: 10, alignItems: "center", marginBottom: 10 }}
+          >
+            <Image
+              source={item.image}
+              style={{
+                width: 50,
+                height: 40,
+                resizeMode: "contain",
+              }}
+            />
+            <Text style={{ fontSize: 12, fontWeight: "700" }}>
+              {items[index].text}
+            </Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
-  )
+  );
 }
